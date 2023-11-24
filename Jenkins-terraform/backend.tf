d69fdbe7608ab030daef42684fe3815dbc2a2c92
shravansahan <shravansahan@gmail.com>
2023-11-24 09:28:49 +0530

    I have changed the region and jenkins installation commands - 1

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "shravan" # Replace with your actual S3 bucket name
+    bucket = "shravan-for-practice-purpose" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-southeast-1"
   }
